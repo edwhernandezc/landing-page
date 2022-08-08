31834bf2e57da9c07da3a5b1a7458e8b4ea4fe1a
Edwin Hernández Cabrera <36483000+edwhernandezc@users.noreply.github.com>
2022-08-07 20:55:40 -0500

    Update assets

--- assets/js/main.js
+++ assets/js/main.js
@@ -194,6 +194,28 @@
       nextEl: ".swiper-button-next",
       prevEl: ".swiper-button-prev",
     },
+    breakpoints: {
+      // when window width is >= 320px
+      320: {
+        slidesPerView: 1,
+        spaceBetween: 20
+      },
+      // when window width is >= 480px
+      480: {
+        slidesPerView: 2,
+        spaceBetween: 30
+      },
+      // when window width is >= 780px
+      780: {
+        slidesPerView: 3,
+        spaceBetween: 30
+      },
+      // when window width is >= 1000px
+      1000: {
+        slidesPerView: 4,
+        spaceBetween: 40
+      }
+    }
   });
 
   /**
